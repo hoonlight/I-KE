78778fea1f0b22efaf9d248d4866e9815d0cb203
hoonlight <kihoon.kim.dev@gmail.com>
2024-08-14 01:30:19 +0900

    Improve error handling and code cleanup

--- i_ke/scraper.py
+++ i_ke/scraper.py
@@ -11,18 +11,8 @@ def get_product_info(url: str) -> dict:
 
     try:
         soup = bs4.BeautifulSoup(response.text, "html.parser")
-    except Exception as e:
-        print(e)
-        return {"manual": None, "stock_button": None}
-
-    try:
         manual = soup.find("div", class_="txt-manual")
         manual = manual.get_text().strip()
-    except Exception as e:
-        print(e)
-        return {"manual": None, "stock_button": None}
-
-    try:
         stock_button = soup.find("div", class_="btn_choice_box btn_restock_box")
         stock_button = stock_button.get_text().strip()
     except Exception as e:
